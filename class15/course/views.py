7bb10d6d848716a232403efc8b9771e34e339ca9
Guru <brluckyjat0789@gmail.com>
2023-10-02 09:52:11 +0530

    frist commit

---
+++ class15/course/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+
+def index(request):
+    return HttpResponse('Home Page')
+
+def learn_django(request):
+    return HttpResponse('<h1>"Hello Django"</h1>')
+
+def learn_python(request):
+    return HttpResponse('<h1>Hello Python</h1>')
